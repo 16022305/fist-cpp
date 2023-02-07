a974f39fa81829fbf5baed09e8baf085d2abfaa7
NTD151298 <duongtn1512@gmail.com>
2023-02-07 17:21:27 +0700

    initial commit

---
+++ 11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main () {
+    for (int i = 0; i < 1; i++){
+        std::cout << std::endl;
+    }
+
+    cout << "Hello 11" << "\n";
+
+    for (int i = 0; i < 1; i++){
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
